52aaf0e778fc75ea46c2df095a36bb0639a5c2d8
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-04-16 22:11:06 -0300

    Java - Clase 02 - TestEnumeraciones Update - Prueba Continentes

--- Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
+++ Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -1,13 +1,36 @@
 
 package test;
 
+import enumeraciones.Continentes;
 import enumeraciones.Dias;
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        System.out.println("Dia 1: "+Dias.LUNES);//Las enumeraciones se tratan como cadenas
-        indicarDiaSemana(Dias.MARTES);
-    //No se deven utilizar comillas, se acceden a traves del operador de punto    
+//        System.out.println("Dia 1: "+Dias.LUNES);//Las enumeraciones se tratan como cadenas
+//        indicarDiaSemana(Dias.MARTES);
+    //No se deven utilizar comillas, se acceden a traves del operador de punto
+    
+        System.out.println("Continentes N° 4: " + Continentes.AMERICA);
+        System.out.println("N° de países en el 1er continente: "
+                + Continentes.AFRICA.getPaises());
+        System.out.println("N° de habitantes en el 1er continente: "
+                + Continentes.AFRICA.getHabitantes());
+        System.out.println("N° de países en el 2do continente: "
+                + Continentes.EUROPA.getPaises());
+        System.out.println("N° de habitantes en el 2do continente: "
+                + Continentes.EUROPA.getHabitantes());
+        System.out.println("N° de países en el 3er continente: "
+                + Continentes.ASIA.getPaises());
+        System.out.println("N° de habitantes en el 3er continente: "
+                + Continentes.ASIA.getHabitantes());
+        System.out.println("N° de países en el 4to continente: "
+                + Continentes.AMERICA.getPaises());
+        System.out.println("N° de habitantes en el 4to continente: "
+                + Continentes.AMERICA.getHabitantes());
+        System.out.println("N° de países en el 5to continente: "
+                + Continentes.OCEANIA.getPaises());
+        System.out.println("N° de habitantes en el 5to continente: "
+                + Continentes.OCEANIA.getHabitantes());
     }
     public static void indicarDiaSemana(Dias dias){
         switch(dias){
