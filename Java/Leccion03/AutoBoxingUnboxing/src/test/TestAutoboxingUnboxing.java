79b212714b48db06bd5a3714bc7d77a2d8887d0a
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-05-05 20:23:39 -0300

    Java - Clase 03 - Clase TestAutoboxingUnboxing - Proyecto AutoboxingUnboxing

---
+++ Java/Leccion03/AutoBoxingUnboxing/src/test/TestAutoboxingUnboxing.java
@@ -0,0 +1,20 @@
+
+package test;
+
+public class TestAutoboxingUnboxing {
+    public static void main(String[] args) {
+        
+        // Clase Envolventes o Wrapper
+    /*
+        Clases envolventes de tipos primitivos
+        int = la clase envolvente es Integer
+    */
+    
+    int enteroPrim = 10; // Tipo Primitivo
+        System.out.println("enteroPrim = " + enteroPrim);
+    Integer entero = 10; // Tipo object con la clase Integer
+        System.out.println("entero = " + entero.toString());
+        
+    }
+    
+}
