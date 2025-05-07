e0281b55eaa8eaf57d62ee6a56337a6214d99a93
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-05-07 12:43:48 -0300

    Java - Clase 04 - Update Class TestSobreescritura

--- Java/Leccion04/Sobreescritura/src/test/TestSobreescritura.java
+++ Java/Leccion04/Sobreescritura/src/test/TestSobreescritura.java
@@ -10,13 +10,14 @@ public class TestSobreescritura {
         Empleado empleado1 = new Empleado("Juan", 10000);
         imprimr(empleado1);
         //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
-        Gerente gerente1 = new Gerente("José", 5000, "Sistemas");
-        imprimr(gerente1);
+        empleado1 = new Gerente("José", 5000, "Sistemas");
+        imprimr(empleado1);
         //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
     }
     
     public static void imprimr(Empleado empleado){
-        System.out.println("empleado = " + empleado.obtenerDetalles());
+        String detalles = empleado.obtenerDetalles();
+        System.out.println("detalles = " + detalles);
     }
     
 }
