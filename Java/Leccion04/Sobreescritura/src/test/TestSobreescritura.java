3658508f017af5c2ae7243af450bc87a181e3420
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-05-07 11:38:25 -0300

    Java - Clase 04 - Polimorfismo - Update Class TestSobreescritura

--- Java/Leccion04/Sobreescritura/src/test/TestSobreescritura.java
+++ Java/Leccion04/Sobreescritura/src/test/TestSobreescritura.java
@@ -2,12 +2,21 @@
 
 package test;
 
-import domain.Gerente;
+import domain.*;
 
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Ariel", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+        Empleado empleado1 = new Empleado("Juan", 10000);
+        imprimr(empleado1);
+        //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+        Gerente gerente1 = new Gerente("José", 5000, "Sistemas");
+        imprimr(gerente1);
+        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
     }
+    
+    public static void imprimr(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+    }
+    
 }
