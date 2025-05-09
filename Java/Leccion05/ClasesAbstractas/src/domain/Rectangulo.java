32b0c2c8c4fd35db9ce22d2e87af0a2f63ea190c
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-05-09 12:29:15 -0300

    Java - Clase 05 - Proyect ClasesAbstractas

---
+++ Java/Leccion05/ClasesAbstractas/src/domain/Rectangulo.java
@@ -0,0 +1,17 @@
+
+
+package domain;
+
+
+public class Rectangulo extends FiguraGeometrica {
+    // Constructor
+    public Rectangulo(String tipoFigura){
+        super(tipoFigura);
+    }
+    
+    @Override
+    public void dibujar(){ // Implementando el método
+        System.out.println("Se imprime un: " + this.getClass().getSimpleName());
+    }
+    
+}
