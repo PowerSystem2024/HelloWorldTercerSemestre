3d9aa9d0e235ea539614622b1cec73710307c01f
joaquin <joakoperaltacorbellini@gmail.com>
2025-05-20 11:55:34 -0300

    clase 7

---
+++ Python/clase7-Logging-Postgresql/capa_datos_persona/logger_base.py
@@ -0,0 +1,21 @@
+import logging as log
+
+# configuración básica
+log.basicConfig(level = log.DEBUG,
+                format = '%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler('python/clase7-Logging-Postgresql/capa_datos_persona/capa_datos.log'),
+                    log.StreamHandler()
+                ])
+
+# crear un logger
+logger = log.getLogger(__name__)
+
+if __name__ == '__main__':
+    log.debug('Mensaje a nivel de debug')
+    log.info('Mensaje a nivel de info')
+    log.warning('Mensaje a nivel de warning')
+    log.error('Mensaje a nivel de error')
+    log.critical('Mensaje a nivel critico')
+
