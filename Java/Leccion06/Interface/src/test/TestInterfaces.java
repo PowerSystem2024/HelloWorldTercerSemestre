7e205e370771fe5657f1659350418d04cf8864c5
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-05-27 12:52:00 -0300

    Java - Clase 07 - Proyecto Interface - Clase TestInterfaces Update

--- Java/Leccion06/Interface/src/test/TestInterfaces.java
+++ Java/Leccion06/Interface/src/test/TestInterfaces.java
@@ -7,6 +7,15 @@ public class TestInterfaces {
     public static void main(String[] args) {
         // Se instancia desde la implementacion no desde la interface
         IAccesoDatos datos = new ImplementacionMySql();
+        //datos.listar();
+        //imprimir(datos);
+        datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+    
+    public static void imprimir(IAccesoDatos datos){
         datos.listar();
     }
+    
 }
