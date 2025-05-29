c69c01276f657dcb0ea8784e1741fda91d4a4a67
joaquin <joakoperaltacorbellini@gmail.com>
2025-05-28 21:31:05 -0300

    cambios menores

--- avatares/public/js/avatar.js
+++ avatares/public/js/avatar.js
@@ -65,7 +65,7 @@ function seleccionarPersonajeEnemigo() {
     }
 }
 
-// Función para generar un número aleatorio entre min y max
+// Función para generar un número aleatorio entre min y max, segun el numero selecciona un personaje
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
